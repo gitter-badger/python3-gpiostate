54237d0697045119d2bc9381dba0c87ca3cb4bcc
scott snyder <scott@growmaster420.com>
2017-02-15 23:16:59 -0800

    test

--- gpiostate.py
+++ gpiostate.py
@@ -7,7 +7,7 @@ def read(pin):
 def export(pin):
     #import os
     pin1f = str(pin)
-    var9978 = ('echo', pin, '> /sys/class/gpio/export')
+    var9978 = ('echo ' + pin1f + ' > /sys/class/gpio/export')
     var66343 = ('echo out > /sys/class/gpio/gpio' + pin1f + '/direction')
     print(var9978)
     print(var66343)
