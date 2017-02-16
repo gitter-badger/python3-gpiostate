8100b72c90e4ea4bded013e40602344b14b2c764
scott snyder <scott@growmaster420.com>
2017-02-15 23:06:18 -0800

    test

--- test.py
+++ test.py
@@ -2,10 +2,12 @@ import gpiostate
 
 
 varf1 = int(input("pin to test? \n"))
-gpiostate.export(varf1)
-gpiostate.read(varf1)
-print(gpiostate.state)
-
+ch1 = input("export or read? \n")
+if ch1 == "read":
+    gpiostate.read(varf1)
+    print(gpiostate.state)
+if ch1 == "export":
+    gpiostate.export(varf1)
 
 '''
 
