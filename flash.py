fc76e18e0f11067af7bdb599e91bb6dbe48b663b
scott snyder <scott@growmaster420.com>
2017-02-16 17:51:42 -0800

    test

--- flash.py
+++ flash.py
@@ -1,10 +1,12 @@
 import gpiostate
 import time
 
-gpiostate.export(26, "out")
+pinV = 26
+
+gpiostate.export(pinV, "out")
 
 for x in range(0,100):
-    gpiostate.flip(26)
+    gpiostate.flip(pinV)
     time.sleep(0.023)
 
-gpiostate.unexport(26)
+gpiostate.unexport(pinV)
