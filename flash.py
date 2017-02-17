b3e97f8b0d74bef9d477016c1f86e74fbc833ede
scott snyder <scott@growmaster420.com>
2017-02-16 19:54:13 -0800

    test

--- flash.py
+++ flash.py
@@ -7,6 +7,6 @@ gpiostate.export(pinV, "out")  # export the pin, and set direction in or out
 
 for x in range(0,100):
     gpiostate.flip(pinV)  # using flip to flash an led
-    time.sleep(0.1)
+    time.sleep(0.05)
 
 gpiostate.unexport(pinV)  # script is done unexport the pin
