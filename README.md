96343cb1a14ad9462dcb8ddc5ecf500d2aa07033
scott snyder <scott@growmaster420.com>
2017-02-16 16:17:10 -0800

    test

--- README.md
+++ README.md
@@ -4,26 +4,28 @@ My first Library!!
 
 simple library to read and write gpio state
  
+pins use BCM values 
+
 ## use
 
     import gpiostate
     
-    #export (must be root)
+    ###export### (must be root)
     
     gpiostate.export(pin) # pin must be exported to read or write 
     
-    # read state
+    ###read state###
     gpiostate.read(pin)
     print(gpiostate.state)
     
-    # write state (must be root)
+    ###write state###(must be root)
     gpiostate.write(pin, stateW) # stateW [on/off]
     
-    # write on (must be root)
+    ###write on###(must be root)
     gpiostate.write(pin, on)
  
-    # write off (must be root)
+    ###write off###(must be root)
     gpiostate.write(pin, off)
     
-    # flip state (must be root)
+    ###flip state###(must be root)
     gpiostate.flip(pin)  
--- test.py
+++ test.py
@@ -1,20 +1,20 @@
 import gpiostate
-varf1 = int(input("pin to test? \n"))
+varf1 = int(input("\npin to test? \n"))
 while True:
-    ch1 = input("export or read or write or flip or flash? \n")
+    ch1 = input("\nexport or read or write or flip or flash? \n")
     if ch1 == "read":
         gpiostate.read(varf1)
         print(gpiostate.state)
     if ch1 == "export":
         gpiostate.export(varf1)
     if ch1 == "write":
-        vard1 = input("write state\non/off?\n")
+        vard1 = input("\nwrite state\non/off?\n")
         gpiostate.write(varf1, vard1)
     if ch1 == "flip":
         gpiostate.flip(varf1)
     if ch1 == "flash":
-        var37 = input("duration of sleep")
-        var38 = input("times to flash")
+        var37 = input("\nduration of sleep?\n")
+        var38 = input("\ntimes to flash?\n")
         gpiostate.flash(varf1, var37, var38)
 
 
