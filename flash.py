918d1f969c128833387568720d254d7ae3b92176
scott snyder <scott@growmaster420.com>
2017-02-16 17:59:41 -0800

    test

--- flash.py
+++ flash.py
@@ -1,12 +1,12 @@
 import gpiostate
 import time
 
-pinV = 26
+pinV = 26  # set pin BCM value
 
 gpiostate.export(pinV, "out")  # export the pin, and set direction in or out
 
 for x in range(0,100):
     gpiostate.flip(pinV)  # using flip to flash an led
-    time.sleep(0.023)
+    time.sleep(0.1)
 
 gpiostate.unexport(pinV)  # script is done unexport the pin
